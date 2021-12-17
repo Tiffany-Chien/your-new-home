e6d2818e89b1ae410aca07b7680481c517881fb2
Tiffany <tiffanychien9@gmail.com>
2021-12-17 15:04:58 -0500

    profile

---
+++ yourNewHome/ProfileTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileTableViewController.swift
+//  yourNewHome
+//
+//  Created by Tiffany Chien on 12/17/21.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 4
+    }
+
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+
+    
+
+}
