7c651049b1c7e3592ba7a0f8130fa38e192a95bd
Tiffany <tiffanychien9@gmail.com>
2021-12-17 15:20:53 -0500

    segue

--- yourNewHome/ProfileTableViewController.swift
+++ yourNewHome/ProfileTableViewController.swift
@@ -13,16 +13,6 @@ class ProfileTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 4
-    }
-
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
 
     
 
